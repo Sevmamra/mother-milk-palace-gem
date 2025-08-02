8f2ba6ee2a51eb65c7031f8bbee52f57a1eb4ee1
Sevmamra <sevmamra01@gmail.com>
2025-08-02 19:06:05 +0530

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
 import CategorySection from '../components/CategorySection';
+import FeaturedProducts from '../components/FeaturedProducts';
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <CategorySection />
-      {/* Other sections like Best Sellers, Offers, etc. will go here */}
+      <FeaturedProducts />
     </>
   );
 };
