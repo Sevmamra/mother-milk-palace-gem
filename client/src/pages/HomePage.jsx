4268e288c1bbe58f48bf17b6bd8f7f36b0ccbd9d
Sevmamra <sevmamra01@gmail.com>
2025-08-02 19:01:20 +0530

    Create HomePage.jsx

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeroSection from '../components/HeroSection';
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroSection />
+      {/* Other sections like categories, best sellers etc. will go here */}
+    </>
+  );
+};
+
+export default HomePage;
